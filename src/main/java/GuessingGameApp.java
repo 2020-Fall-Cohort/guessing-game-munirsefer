f89b7b3f90ef6046b30a7b56b6f704f1712e2c6b
Munir Sefer <munirsefer@gmail.com>
2020-09-10 22:20:27 -0400

    finished first iteration

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("please guess a number between 1-10:");
+        int guess = input.nextInt();
+        if (guess == 7) {
+            System.out.println("You win");
+        } else {
+            System.out.println("You lose");
+        }
     }
 
 }
